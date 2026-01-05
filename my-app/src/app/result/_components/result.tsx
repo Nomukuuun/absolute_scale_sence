6e64f866772df8fef7bc5e1f83a1a719d2af6487
Nomukuuun <nomukuuun5@gmail.com>
2026-01-05 13:26:34 +0900

    ✨Add: スコアによる結果分岐実装

---
+++ my-app/src/app/result/_components/result.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useScore } from '@/app/ScoreProvider'
+import { getResult } from '@/lib/result/get-result'
+
+export function Result() {
+  const { score, reset } = useScore()
+  const result = getResult(score)
+
+  return (
+  <div className="flex flex-col items-center justify-center space-y-6">
+    <div>結果発表～～～～～～！！！！</div>
+    <div className="flex items-center">
+      <div>あなたはスケール感覚は </div>
+      <div className="text-2xl font-bold">{result.label}</div>
+    </div>
+    <div className="whitespace-pre-line">{result.message}</div>
+  </div>
+  )
+}
