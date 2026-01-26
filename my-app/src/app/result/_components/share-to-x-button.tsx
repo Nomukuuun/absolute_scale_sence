4b6a81ad98527cc65a45d1275d94138f7a5f5675
Nomukuuun <nomukuuun5@gmail.com>
2026-01-26 11:08:29 +0900

    🐛Fix: シェア時のメッセージとURLを修正

--- my-app/src/app/result/_components/share-to-x-button.tsx
+++ my-app/src/app/result/_components/share-to-x-button.tsx
@@ -5,22 +5,25 @@ import { useMemo } from 'react'
 import { useScore } from '@/app/ScoreProvider'
 import { getResult } from '@/lib/result/get-result' 
 import { ShareMessage } from '@/lib/result/share-message' 
-import { usePathname } from "next/navigation"
-
 
 export function ShareToXButton() {
+  // 絶対パスでルートを取得し、シェアURLを生成
+  const rootPath = process.env.NEXT_PUBLIC_SITE_URL ?? window.location.origin
+  const shareUrl = new URL('/', rootPath).toString()
+
+  // スコアに基づいてシェアテキストを生成
   const { score } = useScore()
   const result = useMemo(() => getResult(score), [score])
-  const shareText = useMemo(() => `私の絶対スケール感は「${result.label}」でした！\n${ShareMessage[result.label]}`, [result])
-  const pathname = usePathname();
-
+  const shareText = useMemo(
+    () => 
+      `私の絶対スケール感は「${result.label}」でした！\n${ShareMessage[result.label]}\nあなたのスケール感を調べてみよう！`
+    , [result])
+  
   return (
     <TwitterShareButton
       className="flex items-center gap-2"
-      url={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-        shareText
-      )}&url=${encodeURIComponent(pathname)}`}
-      title={'あなたのスケール感を調べてみよう！'}
+      url={shareUrl}
+      title={shareText}
     >
       <div className="flex items-center justify-center gap-2 bg-black text-white rounded-md px-4 py-2">
         <TwitterIcon size={24} round />
