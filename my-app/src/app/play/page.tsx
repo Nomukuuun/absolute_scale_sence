77ae6892244e1dec10f91ca72a8e9c9469d8c402
Nomukuuun <nomukuuun5@gmail.com>
2026-02-01 15:50:18 +0900

    🐛Fix: キャッシュ制御を見直し問題を動的に表示できるように修正

--- my-app/src/app/play/page.tsx
+++ my-app/src/app/play/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from 'next/navigation'
 
+// Full Route Cacheを無効にして動的処理を強制する
+export const dynamic = 'force-dynamic'
+
 export default function PlayIndexPage() {
   const QUESTIONS_STOCKS = 10
   const randomId = Math.floor(Math.random() * QUESTIONS_STOCKS + 1)
